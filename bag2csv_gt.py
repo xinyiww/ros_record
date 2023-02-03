c5d33b23ad2888785de711aec1d8213a5e6a2abb
Xinyi Liu <xliu@sv870501lx.am.mds.honda.com>
2023-02-03 09:38:23 -0800

    fix some bugs in bag2csv_gt, more consistant

--- bag2csv_gt.py
+++ bag2csv_gt.py
@@ -11,16 +11,20 @@ import numpy as np
 # filename = sys.argv[2]
 # directory = sys.argv[1]
 
-def assign_id(x,y, pre_pos_dict, max_dx, max_dy):
+
+
+
+def assign_id(x,y, t, pre_pos_dict, max_dx, max_dy, max_dt):
   n_in_scope = 0
   best_id = None
   min_dist = math.inf
   for i, (veh_id, pre_pos) in enumerate(pre_pos_dict.items()):
     x_dist = abs(pre_pos[0] -  x)
     y_dist = abs(pre_pos[1] - y)
-    if (x_dist <= max_dx) and (y_dist <= max_dy): 
+    t_dist = abs(pre_pos[2] - t)
+    if (x_dist <= max_dx) and (y_dist <= max_dy) and (t_dist <= max_dt): 
       n_in_scope += 1
-      dist = math.sqrt((pre_pos[0] -  x)**2 + (pre_pos[1] -  y)**2)
+      dist = math.sqrt(x_dist**2 + y_dist**2)
       min_dist = np.min([dist, min_dist])
       ids = [veh_id, best_id]
       best_id = ids[np.argmin([dist, min_dist])]
@@ -65,21 +69,27 @@ with open(results_dir +"/"+filename+'_gt.csv', 'a', newline='') as gt_csv:
         
         if pre_pos == {}: # first round, build the first veh_id dic
           for i, lane in enumerate(msg.vehicles):            
-              for j, veh in enumerate(lane.vehicles):
+              for j, veh in enumerate(lane.vehicles): # some deplicates in the first round -- delete them
                   pos = veh.pose.pose.position   
                   log_writer.writerow([t, veh_id, pos.x, pos.y] )
                   
-                  veh_id += 1
-                  pre_pos [veh_id] = [pos.x, pos.y]
+                  if [pos.x, pos.y, t] not in pre_pos.values():
+                    pre_pos[veh_id] = [pos.x, pos.y, t]
+                    veh_id += 1
+                  
+                  
+                  
         else: # else: assign veh_id based on the position correspondence
-          pre_pos_old = pre_pos.copy()
+          
           for i, lane in enumerate(msg.vehicles):            
               for j, veh in enumerate(lane.vehicles):
+                  pre_pos_old = pre_pos.copy()
                   pos = veh.pose.pose.position   
-                  veh_id = assign_id(pos.x, pos.y, pre_pos_old, 1e-3, 1) # (1e-3, -0.4693756103515625)
+                  veh_id = assign_id(pos.x, pos.y,t, pre_pos_old, 1e-3, 1, 0.2) # (1e-3, -0.4693756103515625)
                   log_writer.writerow([t, veh_id, pos.x, pos.y] )
                   
-                  pre_pos [veh_id] = [pos.x, pos.y]
+                  if [pos.x, pos.y, t] not in pre_pos.values():
+                    pre_pos[veh_id] = [pos.x, pos.y, t]
                   
                 
     # pred_csv.close()
